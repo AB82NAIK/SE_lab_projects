040b6c0ff50cf7d77c7ff8a260eb489f67d39843
AB82NAIK <abhishek08naik@gmail.com>
2015-12-21 12:57:59 +0530

    12

---
+++ test/src/test1/pring.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class pring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test");
+		System.exit(0);
+	}
+
+}
